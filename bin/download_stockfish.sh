6f0dc87d814f55b70175699733bdba89e541bae3
Akshaya7483 <akshayas.bsc23@rvu.edu.in>
2025-06-30 11:57:26 +0530

    Rename script to download_stockfish.sh

--- bin/download_stockfish.sh
+++ bin/download_stockfish.sh
@@ -2,16 +2,16 @@
 
 mkdir -p bin
 
-# Download Stockfish 16 Linux AVX2
+# Download Stockfish Linux binary zip
 wget -O bin/stockfish.zip https://stockfishchess.org/files/stockfish_16_linux_x64_avx2.zip
 
-# Unzip
+# Unzip it
 unzip -o bin/stockfish.zip -d bin
 
 # Make sure it's executable
 chmod +x bin/stockfish/stockfish
 
-# Copy to ./bin/stockfish so your code can call ./bin/stockfish
+# Copy to bin/stockfish so your code can call ./bin/stockfish
 cp bin/stockfish/stockfish ./bin/stockfish
 
 echo "Stockfish 16 downloaded and ready."
