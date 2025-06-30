d697e8ca1009964b2668dfaceb4d984d2a5bafb5
Akshaya7483 <akshayas.bsc23@rvu.edu.in>
2025-06-30 12:09:53 +0530

    Fix working download link and permissions

--- bin/download_stockfish.sh
+++ bin/download_stockfish.sh
@@ -2,16 +2,17 @@
 
 mkdir -p bin
 
-# Download Stockfish Linux binary zip from GitHub Releases
-wget -O bin/stockfish.zip https://github.com/official-stockfish/Stockfish/releases/download/sf_16/stockfish-ubuntu-20.04.zip
+# Download the working Stockfish zip
+wget -O bin/stockfish.zip https://stockfishchess.org/files/stockfish_16_linux_x64_avx2.zip
 
 # Unzip it
 unzip -o bin/stockfish.zip -d bin
 
+# The binary ends up inside a folder, e.g., stockfish_16_x64_avx2/stockfish_16_x64_avx2
+# Move it to bin/stockfish
+mv bin/stockfish_16_x64_avx2/stockfish_16_x64_avx2 bin/stockfish
+
 # Make sure it's executable
-chmod +x bin/stockfish/stockfish
+chmod +x bin/stockfish
 
-# Copy to ./bin/stockfish
-cp bin/stockfish/stockfish ./bin/stockfish
-
-echo "Stockfish 16 downloaded and ready."
+echo "Stockfish downloaded and ready."
